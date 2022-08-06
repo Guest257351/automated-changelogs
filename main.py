7fe0d47f0a71782b52f849e473ccbfd10c3f0b7d
Guest257351 <57662212+Guest257351@users.noreply.github.com>
2022-08-06 18:05:28 +0930

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import requests
+import json as js
 
 
 def main():
-    commits = os.environ["commits"]
+    with open(os.environ['GITHUB_EVENT_PATH'], 'r') as f:
+        commits = js.loads(f.read())['commits']
 
-    version = os.environ["versionNo"]
+    version = os.environ["GITHUB_RUN_NUMBER"]
 
-    messages = '\n'.join([i.message for i in commits])
+    messages = '\n'.join([i['message'] for i in commits])
 
-    changelog = f"""Version {version}
+    changelog = f"""Version `{version}`
 ```md
 {messages}
 ```
 """
+    json = {
+      "content": changelog,
+      "embeds": None,
+      "attachments": []
+      }
 
-    print(f"::set-output name=myOutput::{changelog}")
+    requests.post("https://discord.com/api/webhooks/1005383165013135420/Y3AqDuwTIMTC3hdAN2nl9ZK0QAyCFGwsuX-UQyShh0TWaCabsG8C7MeSnKwBP8G6A-5n", json=json)
 
 
 if __name__ == "__main__":
